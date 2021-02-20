d9a7b217f80f833b8bbae3d2804b836b2dd164cc
ulixdeveloppment <77122184+ulixdeveloppment@users.noreply.github.com>
2021-02-20 18:03:11 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("sw.js");
+//enregistrement du service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        // L'enregistrement a fonctionné
+        console.log("Enregistrement du service worker réussi pour le périmètre : ", registration.scope);
+      }, function(err) {
+        // L'enregistrement a échoué :(
+        console.log("Échec de l'enregistrement du service worker: ", err);
+      });
+    });
 }
+
+//app principale
 var showed = 0;
 function eventClick(eventObj){
     if(showed==0){
         console.log(showed);
-        document.getElementById('footer').style.top  = "80%";
+        document.getElementById('footer').style.top  = "85%";
         showed=1;
     }
     else{
@@ -14,3 +25,12 @@ function eventClick(eventObj){
         showed=0;
     }
 }
+function preventDefault(e){
+    e.preventDefault();
+}
+
+function disableScroll(){
+    document.body.addEventListener('touchmove', preventDefault, { passive: false });
+}
+
+
